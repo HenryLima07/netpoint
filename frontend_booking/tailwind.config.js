605e2d0612cb3dfb8a3003652dd4fce28e710689
HenryLima07 <00033721@uca.edu.sv>
2023-09-16 20:47:43 -0600

    start netpoint frontend

---
+++ frontend_booking/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      "lime-yellow": "#CBF51A",
+      "usafa-blue": "#004398",
+      "lapis-lazuli": "#26649B",
+      "neon-sky": "#229CFF",
+      "pure-white": "#FFFFFF",
+    }
+  },
+  plugins: [],
+}
