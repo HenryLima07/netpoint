04987fffd185646d9ebba88e273aec921ba7736d
HenryLima07 <00033721@uca.edu.sv>
2023-09-22 16:14:47 -0600

    add web configuration

--- frontend_booking/tailwind.config.js
+++ frontend_booking/tailwind.config.js
@@ -9,6 +9,7 @@ export default {
       boxShadow: {
         "rounded-button-yellow": "7px 7px 0px 0px #FFDE59",
         "rounded-button-sky": "5px 5px 0px 0px #229CFF",
+        "rounded-courts-location": "17px 17px 0px -3px #000000",
       }
     },
     colors: {
@@ -20,7 +21,8 @@ export default {
       "pure-white": "#FFFFFF",
       "dark-gray": "#2E3646",
       "light-gray": "#5F6D7E",
-      "black": "#000000"
+      "black": "#000000",
+      "neutral": "#E6E9EC"
 
     },
     fontFamily: {
