6fbee13678dc0e96f82272baec734ec7f12c66b1
HenryLima07 <00033721@uca.edu.sv>
2023-09-22 16:18:09 -0600

    add view home

--- frontend_booking/src/view/Home.jsx
+++ frontend_booking/src/view/Home.jsx
@@ -1,12 +1,22 @@
 import Header from "../component/Header/Header";
 import Footer from "../component/Footer/Footer";
 import LandingContainer from "../component/Home/LandingContainer/LandingContainer";
+import ServicioSection from "../component/Home/ServicioSection/ServiciosContainer";
+import ClasesSection from "../component/Home/ClasesSection/ClasesSection";
+import CourtSection from "../component/Home/CourtSection/CourtSection";
+import PromoSection from "../component/Home/PromoSection/PromoSection";
+import PhraseSection from "../component/Home/PhraseSection/PhraseSection";
 
 const Home = () => {
     return(
-        <div className="">
+        <div className=" flex flex-col">
             <Header />
             <LandingContainer />
+            <ServicioSection />
+            <ClasesSection />
+            <CourtSection />
+            <PromoSection />
+            <PhraseSection />
             <Footer />
         </div>
     );
