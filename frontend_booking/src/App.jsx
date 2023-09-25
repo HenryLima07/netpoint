f9e34e35999ce1f6fc38598b99dd5e2cf3dd0b8e
HenryLima07 <00033721@uca.edu.sv>
2023-09-25 17:32:54 -0600

    adding routes and elements for toastify

--- frontend_booking/src/App.jsx
+++ frontend_booking/src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 
 //views
 import Home from "./view/Home";
+import LoginView from "./view/LoginView";
 
 function App() {
   return (
     <Routes>
 
       <Route path="/" element={<Home/>}/>
+      <Route path="/singin" element={<LoginView/>}/>
 
     </Routes>
   )
--- frontend_booking/src/main.jsx
+++ frontend_booking/src/main.jsx
@@ -5,12 +5,20 @@ import './index.css'
 
 //libraries
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
     <BrowserRouter>
       <App />
+      <ToastContainer
+              theme="light"
+              position="top-right"
+              pauseOnHover="false"
+            />
     </BrowserRouter>
   
   </React.StrictMode>,
