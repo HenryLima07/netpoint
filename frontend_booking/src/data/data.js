3ad548f5a3ad6c39d5cb89aae96ad17c2a4ced5e
HenryLima07 <00033721@uca.edu.sv>
2023-09-25 17:31:18 -0600

    add error messages on form

---
+++ frontend_booking/src/data/data.js
@@ -0,0 +1,10 @@
+const ErrorMessages = {
+    "require": "este campo es requerido",
+
+    /*email*/
+    "validateEmail": "ingresar un correo electrónico válido",
+
+
+
+}
+export default ErrorMessages;
