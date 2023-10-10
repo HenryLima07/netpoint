c0de718370b796703d49be844d539c29b96c4fc5
HenryLima07 <00033721@uca.edu.sv>
2023-10-10 16:25:59 -0600

    udpate view cancha individual

--- frontend_booking/src/view/CanchaInd.view.jsx
+++ frontend_booking/src/view/CanchaInd.view.jsx
@@ -1,14 +1,30 @@
 import Header from "../component/Header/Header";
 import Footer from "../component/Footer/Footer";
-
+import DayCalendar from "../component/CanchaInd/FieldBooking/DayCalendarComponent/DayCalendar.component";
 import CanchaIndComponent from "../component/CanchaInd/CanchaInd.component";
 
+import { useState } from "react";
+
 const CanchaIndView = ()=>{
+    const [ showModalTime, setShowModalTime ] = useState(false);
+    const [ events, setEvents ] = useState([]);
+    const [ startTime, setStartTime ] = useState("");
+    const [ endTime, setEndTime ] = useState("");
+
+    const ToggleModal = (state)=>{
+        setShowModalTime(state);
+    }
+
     return(
-        <main className=" min-h-screen h-screen">
+        <main className= {`min-h-screen h-screen ${showModalTime ? " overflow-hidden": ""}`}>
+            {
+                showModalTime ? 
+                    <DayCalendar ToggleModalHandler={ToggleModal}/>
+                : <></>
+            }
             <Header />
             <div className="h-28"></div>
-            <CanchaIndComponent />
+            <CanchaIndComponent ToggleModalHandler={ToggleModal}/>
             <Footer />
         </main>
     )
