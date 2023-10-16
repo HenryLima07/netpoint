a56b263da9159c668ead2f7b0dd5c12c49238032
HenryLima07 <00033721@uca.edu.sv>
2023-10-16 08:02:37 -0600

    update button props

--- frontend_booking/src/component/Buttons/NoBorderButton/NoBorderButton.jsx
+++ frontend_booking/src/component/Buttons/NoBorderButton/NoBorderButton.jsx
@@ -1,6 +1,6 @@
-const NoBorderButton = ({classNB, children, handleClick = ()=>{}}) => {
+const NoBorderButton = ({className, children, handleClick = ()=>{}}) => {
     return(
-        <button className={` pl-5 pr-2 py-2 rounded-xl hover:cursor-pointer ${classNB}`} onClick={handleClick}>
+        <button className={`rounded-xl hover:cursor-pointer ${className}`} onClick={handleClick}>
             {children}
         </button>
     );
