8aeb95447cb0430267b05917925370f308a083d9
HenryLima07 <00033721@uca.edu.sv>
2023-10-27 09:04:47 -0600

    update shoppingcart displayed elements

---
+++ frontend_booking/src/component/ShoppingCart/ShoppingCart.service.jsx
@@ -0,0 +1,44 @@
+const KEY = "shoppingNTPOINT";
+export const ShoppingCartService = () => {
+  //public handlers ls
+  const addElement = (data) => {
+    let elements = getShoppingCart();
+
+    elements = [...elements, data];
+
+    clearShoppingCart();
+    setShoppingCart(elements);
+
+    return elements;
+  };
+
+  const removeElement = (id) => {
+    let elements = getShoppingCart();
+    if (elements == null || elements.length < 0) return;
+
+    elements = elements.filter((e, index) => index != id);
+
+    clearShoppingCart();
+
+    setShoppingCart(elements);
+  };
+
+  const getElements = () => {
+    return getShoppingCart();
+  };
+
+  const service = {
+    addElement,
+    removeElement,
+    getElements,
+    clearShoppingCart,
+  };
+
+  return service;
+};
+
+//shopping cart handlers for localstorage
+const setShoppingCart = (data) =>
+  localStorage.setItem(KEY, JSON.stringify(data));
+const getShoppingCart = () => JSON.parse(localStorage.getItem(KEY)) || [];
+const clearShoppingCart = () => localStorage.removeItem(KEY);
