a2558458ec574133334dc0efbe204735abcb9e24
HenryLima07 <00033721@uca.edu.sv>
2023-10-27 09:14:11 -0600

    update errors and utilities

--- frontend_booking/src/data/data.js
+++ frontend_booking/src/data/data.js
@@ -1,17 +1,19 @@
 const ErrorMessages = {
-    "require": "este campo es requerido",
+  require: "este campo es requerido",
 
-    /*email*/
-    "validateEmail": "ingresar un correo electrónico válido",
+  /*email*/
+  validateEmail: "ingresar un correo electrónico válido",
 
-    /*String only inputs*/
-    "onlyString": "ingresar únicamente caracteres",
+  /*String only inputs*/
+  onlyString: "ingresar únicamente caracteres",
 
-    /*Number only inputs*/
-    "phoneNumber": "debe de ingresar un número telefónico correcto",
+  /*Number only inputs*/
+  phoneNumber: "debe de ingresar un número telefónico correcto",
 
-    /*Date*/
-    "notDate": "no se puede ingresar una fecha pasada",
+  /*Date*/
+  notDate: "no se puede ingresar una fecha pasada",
 
-}
+  /*overlaps with other user's booking */
+  overlaps: "no se ha podido reservar en estas hora",
+};
 export default ErrorMessages;
--- frontend_booking/src/view/CanchaInd.view.jsx
+++ frontend_booking/src/view/CanchaInd.view.jsx
@@ -9,85 +9,96 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 
-const CanchaIndView = ()=>{
+const CanchaIndView = () => {
+  //states to handle selected events in calendar
+  const [showModalTime, setShowModalTime] = useState(false);
+  const [events, setEvents] = useState([]);
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [title, setTitle] = useState("");
+  const [selectedDate, setSelectedDate] = useState("");
 
-    //states to handle selected events in calendar
-    const [ showModalTime, setShowModalTime ] = useState(false);
-    const [ events, setEvents ] = useState([]);
-    const [ startTime, setStartTime ] = useState("");
-    const [ endTime, setEndTime ] = useState("");
-    const [ title, setTitle ] = useState("");
-    const [ selectedDate, setSelectedDate ] = useState("");
+  //states and elements to handle fetch information courts
+  const { FetchCourt } = BookingFormService();
+  const { id } = useParams();
+  const [data, setData] = useState([]);
+  const navigate = useNavigate();
 
-    //states and elements to handle fetch information courts
-    const { FetchCourt } = BookingFormService();
-    const { id } = useParams();
-    const [ data, setData ] = useState([])
-    const navigate = useNavigate();
+  const ToggleModal = (state) => {
+    setShowModalTime(state);
+  };
 
-    const ToggleModal = (state)=>{
-        setShowModalTime(state);
+  const startTimeHandler = (start) => {
+    setStartTime(start);
+  };
+
+  const endTimeHandler = (end) => {
+    setEndTime(end);
+  };
+
+  const titleHandler = (title) => {
+    setTitle(title);
+  };
+
+  const setEventsHandler = (events) => {
+    setEvents(events);
+  };
+
+  const clearSelectedTime = () => {
+    setStartTime("");
+    setEndTime("");
+  };
+
+  const fetchCourtHandler = async () => {
+    const { data } = await FetchCourt(id);
+    if (!data) {
+      toast.warn("UPS! algo ocurrió mal", { toastId: "warn" });
+      navigate("/cancha");
+      return;
     }
+    setData(data);
+  };
 
-    const startTimeHandler = (start)=>{
-        setStartTime(start);
-    }
+  useEffect(() => {
+    fetchCourtHandler();
+  }, []);
 
-    const endTimeHandler = (end)=>{
-        setEndTime(end);
-    }
-
-    const titleHandler = (title)=>{
-        setTitle(title);
-    }
-
-    const setEventsHandler = (events) =>{
-        setEvents(events);
-    }
-
-    const clearSelectedTime = ()=>{
-        setStartTime("");
-        setEndTime("");
-    }
-
-    const fetchCourtHandler = async()=>{
-        const { data } = await FetchCourt(id);
-        if(!data) {
-            toast.warn("UPS! algo ocurrió mal", { toastId: "warn"});
-            navigate("/cancha");
-            return;
-        }
-        setData(data);
-    }
-
-    useEffect(() => {
-        fetchCourtHandler();
-    }, []);
-
-    return(
-        <main className= {`max-w-[1920px] min-h-screen h-screen ${showModalTime ? " overflow-hidden": ""}`}>
-            {
-                showModalTime ? 
-                    <DayCalendar ToggleModalHandler={ToggleModal} startTimeHandler={startTimeHandler} endTimeHandler={endTimeHandler} title={title} selectedDate={selectedDate} events={events}/>
-                : <></>
-            }
-            <Header />
-            <div className="h-5 md:h-28"></div>
-            <CanchaIndComponent 
-                startTime={startTime} 
-                endTime={endTime} 
-                title={title} 
-                selectedDate={selectedDate} 
-                ToggleModalHandler={ToggleModal} 
-                titleHandler={titleHandler} 
-                dateHandler={setSelectedDate} 
-                setEventsHandler={setEventsHandler} 
-                clearSelectedTimeHandler={clearSelectedTime}
-                data={data}
-                />
-            <Footer />
-        </main>
-    )
-}
+  return (
+    <main
+      className={`max-w-[1920px] min-h-screen h-screen ${
+        showModalTime ? " overflow-hidden" : ""
+      }`}
+    >
+      {showModalTime ? (
+        <DayCalendar
+          ToggleModalHandler={ToggleModal}
+          startTimeHandler={startTimeHandler}
+          endTimeHandler={endTimeHandler}
+          title={title}
+          selectedDate={selectedDate}
+          events={events}
+        />
+      ) : (
+        <></>
+      )}
+      <Header />
+      <div className="h-5 md:h-28"></div>
+      <CanchaIndComponent
+        startTime={startTime}
+        endTime={endTime}
+        title={title}
+        selectedDate={selectedDate}
+        ToggleModalHandler={ToggleModal}
+        titleHandler={titleHandler}
+        dateHandler={setSelectedDate}
+        setEventsHandler={setEventsHandler}
+        clearSelectedTimeHandler={clearSelectedTime}
+        data={data}
+        canchaId={id}
+      />
+      <Footer />
+    </main>
+  );
+};
 
 export default CanchaIndView;
